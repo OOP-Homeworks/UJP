0493d068d7617df31835242539c03730d7eecf88
Yurii-Hubych <90180584+Yurii-Hubych@users.noreply.github.com>
2022-07-14 23:15:26 +0300

    Homework

---
+++ Homework/homework_1/Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace ConsoleApplication4
+{
+    public class Person
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+        }
+        private DateTime birthYear;
+        public DateTime BirthYear
+        {
+            get { return birthYear; }
+        }
+
+        public Person()
+        {
+            name = "Mark";
+            birthYear = new DateTime(2002, 1, 1, 6, 32, 0);
+        }
+
+        public Person(string name, DateTime birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+
+        public static Person Input(int i)
+        {
+            Console.WriteLine($"Enter {i+1} person name");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Enter {i+1} birth year (day/month/year)");
+            DateTime birthYear;
+            DateTime.TryParse(Console.ReadLine(), out birthYear);
+            Person person = new Person(name, birthYear);
+            return person;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public void ChangeName(string name)
+        {
+            this.name = name;
+
+        }
+
+        public static int Age(DateTime birthYear)
+        {
+            DateTime year = DateTime.Now;
+            return year.Year-birthYear.Year;
+        }
+
+        public override string ToString()
+        {
+            return $"Person's name - {name} \t person's birth year is - {birthYear} \t person's Age - {Age(birthYear)}";
+        }
+        
+        public static bool operator ==(Person first, Person second)
+        {
+            return (first.name == second.name);
+        }
+
+        public static bool operator !=(Person first, Person second)
+        {
+            return !(first == second);
+        }
+    }
+}
---
+++ Homework/homework_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person[] persons = new Person[6];
+            for (int i = 0; i < persons.Length; i++)
+            {
+                persons[i] = Person.Input(i);
+            }
+
+            for (int i = 0; i < persons.Length; i++)
+            {
+                persons[i].Output();
+            }
+
+            for (int i = 0; i < persons.Length; i++)
+            {
+                if (Person.Age(persons[i].BirthYear) <16)
+                {
+                    persons[i].ChangeName("Very young");
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < persons.Length; i++)
+            {
+                persons[i].Output();
+            }
+            
+            Console.WriteLine();
+            for (int i = 0; i < persons.Length; i++)
+            {
+                for (int j = i+1; j < persons.Length; j++)
+                {
+                    if (persons[i]==persons[j])
+                    {
+                        Console.WriteLine($"{persons[i]} and \n {persons[j]} \n are equal");
+                    }
+                }
+            }
+        }
+    }
+}
