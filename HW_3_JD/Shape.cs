18ba16e6041211285c5bab66409d9703cca2bb42
Julia <90181030+Juli11V@users.noreply.github.com>
2022-07-20 21:05:46 +0300

    HW_3

---
+++ HW_3_JD/Shape.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public abstract class Shape : IComparable
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Shape(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract double Area();
+        public abstract double Perimeter();
+        public abstract int CompareTo(object obj);
+
+    }
+}
---
+++ HW_3_JD/Square.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Square : Shape
+    {
+        private double side;
+        public double Side { get { return side; } }
+
+        public Square(string name, double side) : base(name)
+        {
+            this.side = side;
+        }
+
+        public override double Area()
+        {
+            return side * side;
+        }
+
+        public override double Perimeter()
+        {
+            return 2 * (side + side);
+        }
+
+        public override string ToString()
+        {
+            return "Name is " + Name + ", side is " + side;
+        }
+
+        public override int CompareTo(object obj)
+        {
+            return this.Perimeter().CompareTo((obj as Shape).Perimeter());
+        }
+    }
+
+}
