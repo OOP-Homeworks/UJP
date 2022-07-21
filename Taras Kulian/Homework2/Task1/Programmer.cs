acd6ec52886b7fa7c114bad33564993fbbbdac0e
foxerest <kuliantaras@gmail.com>
2022-07-21 14:42:57 +0300

    add homework 2

---
+++ Taras Kulian/Homework2/Task1/Programmer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Programmer : IDeveloper, IComparable<IDeveloper>
+    {
+        string language;
+        public string Tool { get => language; set => language = value; }
+
+        public Programmer(string language)
+        {
+            this.language = language;
+        }
+
+        public void Create()
+        {
+            Console.WriteLine($"Hi! I'm a {language} programmer!");
+        }
+
+        public void Destory()
+        {
+            Console.WriteLine($"The {language} programmer needs some rest!");
+        }
+
+        int IComparable<IDeveloper>.CompareTo(IDeveloper? other)
+        {
+            if (other != null)
+            {
+                return String.Compare(this.Tool, other.Tool);
+            } else
+            {
+                throw new ArgumentException("Wrong argument");
+            }
+            
+        }
+    }
+}
---
+++ Taras Kulian/Homework2/Task2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> massages = new();
+            massages.Add("Wrong ID", "Please try again. Enter unsigned integer!");
+            massages.Add("ID is used", "Please try again. Person with that ID " +
+                "already exists!");
+            massages.Add("Wrong name", "Please try again. Field name can't be empty!");
+            massages.Add("Person not found", "Can't find a person with this ID :c");
+
+            const int PEOPLE_AMOUNT = 3;
+            Dictionary<uint, string> people = new();
+            uint inputID;
+            string ?inputName;
+            bool isInputCorrect;
+
+            Console.WriteLine($"Please, enter info 'bout {PEOPLE_AMOUNT} people" +
+                $" in the following form: {{ID, name}}");
+            for (int i = 0; i < PEOPLE_AMOUNT; i++)
+            {
+                Console.WriteLine($"Person #{i + 1}");
+                do
+                {
+                    Console.WriteLine("Enter ID:");
+                    isInputCorrect = uint.TryParse(Console.ReadLine(), out inputID);
+                    if (!isInputCorrect)
+                    {
+                        Console.WriteLine(massages["Wrong ID"]);
+                    } else if (people.ContainsKey(inputID))
+                    {
+                        Console.WriteLine(massages["ID is used"]);
+                        isInputCorrect = false;
+                    }
+                } while (!isInputCorrect);
+                do
+                {
+                    Console.WriteLine("Enter name");
+                    inputName = Console.ReadLine();
+                    isInputCorrect = (inputName != null);
+                    if (!isInputCorrect)
+                    {
+                        Console.WriteLine(massages["Wrong name"]);
+                    }
+                } while (!isInputCorrect);
+               
+                people.Add(inputID, inputName);
+            }
+            do
+            {
+                Console.WriteLine("Please, enter a person's ID you want to find:");
+                isInputCorrect = uint.TryParse(Console.ReadLine(), out inputID);
+                if (!isInputCorrect)
+                {
+                    Console.WriteLine(massages["Wrong ID"]);
+                } else
+                {
+                    if (people.TryGetValue(inputID, out string value))
+                    {
+                        Console.WriteLine($"Person is found: {{ID: {inputID}," +
+                            $"name: {value}}}");
+                    }
+                    else
+                    {
+                        Console.WriteLine(massages["Person not found"]);
+                    }
+                }
+            } while (!isInputCorrect);
+            
+        }
+    }
+}
